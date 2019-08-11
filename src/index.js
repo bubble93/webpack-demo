c6412bb6055d9d0f5cf6c3e72b6035ab02611fdc
bubble <17791205330@163.com>
2019-08-11 15:07:22 +0800

    开发

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ function component() {
   var btn = document.createElement('button');
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  btn.innerHTML = 'Click me and check the console!';
+  btn.innerHTML = 'Click me and check the console!111';
   btn.onclick = printMe;
 
   element.appendChild(btn);
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,12 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js'
+    filename: '[name].bundle.js',
+    publicPath: '/'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
   },
   plugins:[
     new CleanWebpackPlugin(),
